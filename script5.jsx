6fd4b4ebf373559e0b6715223a7b69d682155b6d
JamesMillerDev <jimmymillerdev@gmail.com>
2018-06-05 11:56:32 -0400

    Add files via upload

---
+++ script5.jsx
@@ -0,0 +1,164 @@
+var posts1 = ["How I built an absolutely killer startup with no money or experience whatsoever", "78parkourMasterX", "38",
+"You’re doing marketing all wrong.  Here’s why.", "yudawa", "38",
+"I love to read.  Here’s why.", "laiwser", "97",
+"I’m brilliant, but I have absolutely nothing to show for it.  Here’s why.", "EntelicoFlameran", "382",
+"How I continue to justify my existence", "maldor", "12",
+"The benefits of spaced repetition systems", "exclele", "687",
+"Scientists make breakthrough in optics", "k2", "38",
+"The role of intuition in mathematical proofs", "1one1", "437",
+"Learning to code - 365 days, 365 programs", "something", "12",
+"A brief introduction to copyright law", "4pick7a81number3", "13",
+"The quality of the front page lately", "theSonOf78parkourMasterX", "67",
+"“Life is short”", "bernandus", "10",
+"Who’s hiring right now?", "Rebooted_v2.0", "16",
+"Better algorithms for traffic lights", "callus285471", "67",
+"An introduction to zero knowledge proofs", "MickMan", "786",
+"Understanding behavior, understanding each other", "Cqgy671", "12",
+"Relaxing sports to play on the weekends", "kidFr0g", "67",
+"How I convinced my friends to use a password management system", "Yoroshikeki", "87",
+"It’s never too late to get started", "spladius", "98",
+"Tips on writing a good news headline", "m67j8", "90"]
+
+var posts2 = ["Building a PC has never been easier", "lothaquacious", "38",
+"A brief introduction to the concept of monopolies", "Vinvin", "263",
+"How to choose the best color to wear", "olespres", "78",
+"Why you need an advanced degree", "sicsicUare", "32",
+"My startup is still doing great", "78parkourMasterX", "12",
+"How to identify pseudoscience", "maur", "52",
+"Are you happy with your text editor?", "Rrmcgrrr", "482",
+"How the language you speak shapes your way of thinking", "apis6", "92",
+"A little something I've been working on...", "mrgbb", "190",
+"\"Don't argue with me.\"", "C1", "46",
+"Clean UI design", "dav27", "87",
+"How my obsession with soda machines began", "DotOne", "77",
+"How to get over bad publicity", "Xx317", "41",
+"Algorithms for anti-aliasing", "Cwusom", "48",
+"So, it's come to this...", "BigPassword", "9",
+"Me and you, but most importantly of all, us", "nj", "25",
+"I'm going to tell you what you should do.  Here's why.", "Yavala", "82",
+"Letting go of fear", "sup.inf.lim", "95",
+"Open problems in algebraic geometry", "bizum", "306",
+"How to choose a version number", "Penncsf", "33"]
+
+var posts3 = ["What to do when you're not sure how to feel", "AstediusBunger", "161",
+"You decide what your device does", "balala32", "123",
+"Good books on programming?", "Jack_Nickles", "140",
+"Everything you ever wanted to know about the placebo effect", "jeremb", "27",
+"When will cars fly?", "FrodLeader", "198",
+"Been thinking about switching operating systems...", "staceyj", "17",
+"Open source software is the wave of the future.  Here's why.", "Swan", "41",
+"Why modern UI design is hard.", "M_B", "59",
+"Why academic journal articles deserve to be free", "siwigun", "100",
+"When you're an artist and you're in a rut...", "mBag", "83",
+"How to find the best dentist", "FrontLoader", "42",
+"I want to see if anyone feels the same way...", "buttonMan", "32",
+"Strategies for risk management", "BondOfSilver", "165",
+"What I learned from watching other people", "DavidsGotGame", "107",
+"Numerical algorithms", "3Dvision", "7",
+"How to revive a dead programming language", "MisterGoo", "58",
+"I can't believe I'm actually using a computer!", "kremc", "99",
+"How to get invited to all the coolest parties", "affable2", "53",
+"This is the best I've ever felt in my life.  Here's why.", "perforation", "32",
+"The best way to use numbers", "3ways", "176"]
+
+var posts4 = ["!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN",
+"!!URGENT!! NOTICE FOR THE COMMUNITY FROM THE ADMINISTRATOR -- PLEASE READ --", "admin", "NaN"]
+
+var posts5 = ["How to make a lot of money, fast.  Here's why.", "fenero",
+"Why it's important to follow the rules", "Johnist",
+"Cool magic tricks", "hombakov",
+"Setting up a personal server", "mark8338",
+"Help support local businesses", "aporvamenta",
+"How easy is it to learn social skills?", "sky_blue_red",
+"Sharing is healthy for you", "danba",
+"The government is oppressive and we need to do something about it", "vahat",
+"Uses for pets that you never thought of before", "inthereeds",
+"Who can you trust with your personal data?", "neurotrust47",
+"Is music really worth the money?", "fantasticus",
+"Who else is feeling sad today?", "pimbpimb",
+"You'll never guess the answer to this math problem", "ACCCCCCT",
+]
+
+var posts = posts4
+
+var doc = app.activeDocument;
+var cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+for (var i = 0; i < posts.length; i += 3)
+{
+	var starter_delta = 0;
+	for (var j = cur_file.artLayers.length - 1; j >= 0; --j)
+	{
+		var cur_name = cur_file.artLayers[j].name;
+		cur_file.artLayers[j].duplicate(doc, ElementPlacement.INSIDE);
+		if (cur_name == "number")
+		{
+			cur_file.close();
+			var num = i / 3 + 1;
+			app.activeDocument.artLayers[0].textItem.contents = num.toString() + ".";
+			cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+		}
+		
+		if (cur_name == "headline")
+		{
+			cur_file.close();
+			app.activeDocument.artLayers[0].textItem.contents = posts[i];
+			app.activeDocument.artLayers[0].name = "sequencereddit-";
+			cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+		}
+		
+		if (cur_name == "name")
+		{
+			cur_file.close();
+			var prev = app.activeDocument.artLayers[0].bounds[2];
+			app.activeDocument.artLayers[0].textItem.contents = posts[i + 1];
+			//app.activeDocument.artLayers[0].textItem.convertToShape();
+			starter_delta = app.activeDocument.artLayers[0].bounds[2] - prev;
+			app.activeDocument.artLayers[0].name = ";" + posts[i + 1] + "0-";
+			cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+		}
+		
+		if (cur_name == "|")
+		{
+			cur_file.close();
+			app.activeDocument.artLayers[0].translate(starter_delta, 0);
+			cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+		}
+		
+		if (cur_name == "comments")
+		{
+			cur_file.close();
+			app.activeDocument.artLayers[0].translate(starter_delta, 0);
+			app.activeDocument.artLayers[0].textItem.contents = posts[i + 2] + " comments"
+			app.activeDocument.artLayers[0].name = ";sequencereddit-";
+			cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+		}
+	}
+	
+	cur_file.close();
+	for (var j = 0; j < 7; ++j)
+	{
+		app.activeDocument.artLayers[j].translate(207, 60 * (i / 3) + 135);
+	}
+	
+	cur_file = app.open(new File("C:\\Users\\capta\\reddititem.psd"));
+}
+
+cur_file.close();
